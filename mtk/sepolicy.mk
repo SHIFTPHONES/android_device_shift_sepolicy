e6463b3cafd38d0add29790e5164e6061d290417
Alexander Martinz <amartinz@shiftphones.com>
2018-04-12 14:46:56 +0200

    mtk: move existing policies to board policies
    Change-Id: Ia645d61c1ef9ea2b1be7d293067d017a770b1501
    Signed-off-by: Alexander Martinz <amartinz@shiftphones.com>

--- mtk/sepolicy.mk
+++ mtk/sepolicy.mk
@@ -2,4 +2,4 @@ BOARD_PLAT_PRIVATE_SEPOLICY_DIR += \
     device/shift/sepolicy/mtk/private \
 
 BOARD_SEPOLICY_DIRS += \
-    device/shift/sepolicy/mtk/common \
+    device/shift/sepolicy/mtk/board \
